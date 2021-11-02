0bfac8dd0b97793bc245a727ddb6bb5083c17461
Nima Shadaram <60158066+nimashad16@users.noreply.github.com>
2021-11-02 12:57:59 -0500

    Update project1.c

--- project1.c
+++ project1.c
@@ -1,1 +1,105 @@
+//
+//  main.c
+//  Project1
+//
+//  Created by Nima $wagaram on 11/1/21.
+//  Copyright © 2021 Nima $wagaram. All rights reserved.
+//
+
+#include <stdio.h>
+
+struct info{
+    int count;
+    int num;
+     int numArray[1];
+};
+
+int getPValue(){
+    int check;
+    int num;
+                    num = scanf("%d",&check);
+    
+    return check;
+    }
+    
+int getVoluntary(){
+    int voluntary;
+      int num;
+        num = scanf("%d",&voluntary);
+      
+      return voluntary;
+         }
+
+int getInvoluntary(){
+    int Involuntary;
+         int num;
+         
+                         num = scanf("%d",&Involuntary);
+         
+         return Involuntary;
+            }
+
+int * putValuesinArray(){
+    int multiply = getInvoluntary();
+    int a;
+    
+    int arrayNums[multiply*3];
+    int newNum;
+    int num;
+    while(1)
+    for(a =0; a< multiply *3; a++){
+        num = scanf("%d",&newNum);
+        if(num == EOF || num == 0){
+            break;
+        }
+        else{
+            arrayNums[a] = newNum;
+        }
+    }
+    
+    return 0;
+}
+
+int main(int argc, const char * argv[]) {
+
+    int volInvolCheck;
+    
+    int result = getPValue();                       //Gets the P value
+    int voluntary = getVoluntary();                 //Get voluntary context switch
+    int involuntary = getInvoluntary();             //Get involuntary context switch
+    
+  
+    printf("%d\n",result);                          //P value
+    printf("%d\n",voluntary);                       //Gets voluntary context switch
+    if(voluntary != involuntary){                   //Gets the nonVoluntary switch
+           volInvolCheck = 0;
+        printf("%d\n",volInvolCheck);
+       }
+    else{
+       printf("%d\n",involuntary);
+    }
+    
+    int multiply = getInvoluntary();
+       int a;
+       
+       int arrayNums[multiply*3];
+       int newNum;
+       int num;
+    
+       for(a =0; a< multiply *3; a++){
+           num = scanf("%d",&newNum);
+           if(num == EOF || num == 0){
+               break;
+           }
+           else{
+               arrayNums[a] = newNum;
+               
+           }
+       }
+       
+    
+    printf("100.0\n");                              //CPU Utilization
+    
+    return 0;
+}
 
