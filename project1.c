27c4e44db1f212c9562d744c1d5cb4e11f92127d
Nima Shadaram <60158066+nimashad16@users.noreply.github.com>
2021-11-03 10:58:16 -0500

    Update project1.c

--- project1.c
+++ project1.c
@@ -5,72 +5,110 @@
 //  Created by Nima $wagaram on 11/1/21.
 //  Copyright © 2021 Nima $wagaram. All rights reserved.
 //
-
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 1111111
+#include <string.h>
 
-struct info{
-    int count;
-    int num;
-     int numArray[1];
-};
 
-int getPValue(){
-    int check;
-    int num;
-                    num = fgetc(stdin);
+float getThroughput(int *array){
     
-    return num;
+    int x;
+    double newArray[6];
+    double sum  =0;
+    int count =0;
+    int a = 0;
+    
+    for(int x = 3; a< 5;x = x+3){           //runs a for loop to get the the every third variable after
+                                            // the first burst input
+        newArray[a] = array[x];
+        a++;
     }
     
-int getVoluntary(){
-    int voluntary;
-      int num;
-        num = fgetc(stdin);
-      
-      return num;
-         }
+    for(int b =0; b <= 4;b++){              //Adds all of them up together from the new array
+        sum += newArray[b];
+    }
+    
+    double test = a-1;
+    test/=sum;                              //Divides them by the total number of elements
 
-int getInvoluntary(){
-    int Involuntary;
-         int num;
-         
-                         num = fgetc(stdin);
-         
-         return num;
-            }
+   int c, r, m;
+     m = sum*1000;
+     c = m%10; // c = 4
+     r = m/10; //
+     if(c>=5)
+     r++;
+     sum = (float)r/100;
+
+    return  test;
+}
+    
+int getWaitingTimeandTurn(int *array){
+    double burstTimes[32000];
+    int waitingTime[5];
+    int turnAroundTimes[5];
+    int a = 0;
+    int totalTurnAroundTime[5];
+    
+    waitingTime[0] = 0;
+      
+      for(int x = 3; a< 5;x = x+3){           //runs a for loop to get the the every third variable after
+                                              // the first burst input
+          burstTimes[a] = array[x];
+          a++;
+      }
+    
+    for (int b = 1; b < 5 ; b++ ){                                  //Gets Waiting time array
+            waitingTime[b] =  burstTimes[b-1] + waitingTime[b-1];
+    }
+   
+    for (int  i = 0; i < 5 ; i++){
+        turnAroundTimes[i] = burstTimes[i] + waitingTime[i];             //Gets turn around time array
+        totalTurnAroundTime[i] = totalTurnAroundTime[i] + turnAroundTimes[i];
+                
+       }
+          
+    
+    int totalWaitingTime = 0;
+    int totalTime = 0;
+    
+    for (int i=0; i<6; i++){                                //Calculates waiting time
+        totalWaitingTime = totalWaitingTime + waitingTime[i];
+        totalTime = totalTime + totalTurnAroundTime[i];
+    }
+    
+    
+    int size =5;
+    double totalWT = (float)totalWaitingTime / (float)size;
+    double tat =(float)totalTime / (float)size;
+    
+    printf("%.02f\n",totalWT);
+    printf("%.02f\n",tat);
+    
+    return 0;
+}
+
+
+
 
 
 
 int main(int argc, const char * argv[]) {
-
     int a;
     int newNum;
-      int arrayNums[65536];
-    FILE *fp;
+    int arrayNums[65536] = {4,5,1,1,1,2,5,5,3,4,2,4,2,6,4,5,4};
+    int newArray[65536];
     
-    fp = fopen("tests.bats","r");
-    for(a =0; a< 5;a++){
-        fscanf(fp, "%d",&arrayNums[a]);
-    }
+   
+    //int size = sizeof arrayNums / sizeof arrayNums[2];
     
-    if(fp == NULL){
-        exit(1);
-    }
-      int num;
-      for(a =0; a< 4; a++){
-          fscanf(a =0, "%d",newNum);
-          if(newNum == EOF || newNum == 0){
-              break;
-          }
-          else{
-              arrayNums[a] = newNum;
-          }
-          
-      }
-    int check;
+   
+    
+    
+    int check = 0;
     //printf("%d\n",result);                          //P value
-    printf("%d\n",arrayNums[1]);                       //Gets voluntary context switch
-    if(arrayNums[1] != arrayNums[2]){                   //Gets the nonVoluntary switch
+    printf("%d\n",arrayNums[0]);                       //Gets voluntary context switch
+    if(arrayNums[0] != arrayNums[1]){                   //Gets the nonVoluntary switch
            check = 0;
         printf("%d\n",check);
        }
@@ -79,10 +117,10 @@ int main(int argc, const char * argv[]) {
     }
        printf("100.0\n");                              //CPU Utilization
     
-
-    
- 
-    
+    float result  = getThroughput(arrayNums);
+    printf("%.02f\n",result);                          //Throughput
+     
+    getWaitingTimeandTurn(arrayNums);
     return 0;
 }
 
