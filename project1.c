0d5facd66a9ec1928ee7189a1d4c4eed7964495e
Nima Shadaram <60158066+nimashad16@users.noreply.github.com>
2021-11-04 04:59:30 -0500

    Update project1.c

--- project1.c
+++ project1.c
@@ -7,16 +7,28 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_SIZE 1111111
 #include <string.h>
 
+struct Process{
+    int processQueue[98307];
+    int processId[98307];
+    int PID;
+    int size;
+};
+
+struct ProcessInfo{
+    int array[98307];
+    int size;
+    int counter;
+    int waitingTime;
+    int PID;
+    int totalBurstTime;
+    int burst[98307];
+};
 
 float getThroughput(int *array){
-    
-    int x;
     double newArray[6];
     double sum  =0;
-    int count =0;
     int a = 0;
     
     for(int x = 3; a< 5;x = x+3){           //runs a for loop to get the the every third variable after
@@ -42,71 +54,90 @@ float getThroughput(int *array){
 
     return  test;
 }
+
+
+int getWaitingTime(struct Process processQueue[],struct ProcessInfo array[], int pQSize, int arrSize){
     
-int getWaitingTimeandTurn(int *array){
-    double burstTimes[32000];
-    int waitingTime[5];
-    int turnAroundTimes[5];
-    int a = 0;
-    int totalTurnAroundTime[5];
-    
-    waitingTime[0] = 0;
-      
-      for(int x = 3; a< 5;x = x+3){           //runs a for loop to get the the every third variable after
-                                              // the first burst input
-          burstTimes[a] = array[x];
-          a++;
-      }
-    
-    for (int b = 1; b < 5 ; b++ ){                                  //Gets Waiting time array
-            waitingTime[b] =  burstTimes[b-1] + waitingTime[b-1];
+    int x = 0;
+    int y=0;
+    while(x < arrSize){
+        y = 0;
+        while(y < pQSize && array[x].counter > 0){
+            if (processQueue[y].PID == array[x].PID){
+                array[x].counter -= 1;
+                continue;
+             }
+            array[x].waitingTime += processQueue[y].size;
+            y++;
     }
-   
-    for (int  i = 0; i < 5 ; i++){
-        turnAroundTimes[i] = burstTimes[i] + waitingTime[i];             //Gets turn around time array
-        totalTurnAroundTime[i] = totalTurnAroundTime[i] + turnAroundTimes[i];
-                
-       }
-          
-    
-    int totalWaitingTime = 0;
-    int totalTime = 0;
-    
-    for (int i=0; i<6; i++){                                //Calculates waiting time
-        totalWaitingTime = totalWaitingTime + waitingTime[i];
-        totalTime = totalTime + totalTurnAroundTime[i];
+        x++;
+}
+    int sum =0;
+    for(int a =0; a < arrSize;a++){
+        sum += array[a].waitingTime;
     }
+    sum /= arrSize;
     
-    
-    int size =5;
-    double totalWT = (float)totalWaitingTime / (float)size;
-    double tat =(float)totalTime / (float)size;
-    
-    printf("%.02f\n",totalWT);
-    printf("%.02f\n",tat);
+     return sum;
+}
+
+int getTurnAroundTime(){
     
     return 0;
 }
 
-
-
-
-
+int getResponseTime(struct Process processQueue[],struct ProcessInfo array[], int pQSize, int arrSize){                      //First time - Arrival Time
+        int x = 0;
+        int y=0;
+        while(x < arrSize){
+            y = 0;
+            while(y < pQSize && array[x].counter > 0){
+                if (processQueue[y].PID == array[x].PID){
+                    array[x].counter -= 1;
+                    continue;
+                 }
+                array[x].waitingTime += processQueue[y].size;
+                y++;
+        }
+            x++;
+    }
+        int sum =0;
+        for(int a =0; a < arrSize;a++){
+            sum += array[a].waitingTime;
+        }
+        sum /= arrSize;
+        
+         return sum;
+    }
 
 int main(int argc, const char * argv[]) {
+    int start;
+    
     int a;
-    int newNum;
-    int arrayNums[65536] = {4,5,1,1,1,2,5,5,3,4,2,4,2,6,4,5,4};
-    int newArray[65536];
+     FILE *fp = fopen(fp,"r");
     
-   
-    //int size = sizeof arrayNums / sizeof arrayNums[2];
+    int newP;
+    int numVals;
     
-   
+    struct Process processID[98307];
+    struct ProcessInfo burst[98307];
+    int priorityID[98307];
     
+    fscanf(fp,"%d", &start);
+    fscanf(fp,"%d", &newP);
+    fscanf(fp,"%d", &numVals);
+    
+    
+    for(a =0; a<numVals; ++a ){
+        fscanf(fp,"%d %d %d", &processID,&burst,&priorityID);
+    }
+    
+    int arrayNums[98307] = {4,5,1,1,1,2,5,5,3,4,2,4,2,6,4,5,4};
+    
+    int size = sizeof (burst) / sizeof (burst[0]);
     
     int check = 0;
-    //printf("%d\n",result);                          //P value
+
     printf("%d\n",arrayNums[0]);                       //Gets voluntary context switch
     if(arrayNums[0] != arrayNums[1]){                   //Gets the nonVoluntary switch
            check = 0;
@@ -117,10 +148,17 @@ int main(int argc, const char * argv[]) {
     }
        printf("100.0\n");                              //CPU Utilization
     
+    //
+    
+    printf("%d\n",size);
     float result  = getThroughput(arrayNums);
-    printf("%.02f\n",result);                          //Throughput
-     
-    getWaitingTimeandTurn(arrayNums);
+    printf("%.02f\n",result);
+    float wait = getWaitingTime(processID,burst,numVals,size);
+    printf("0.02f\n",wait);
+                           //Throughput
+   
     return 0;
 }
+
+
 
